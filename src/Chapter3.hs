0cfdbced35c8bb6b750ebe1f44ef9a984b7960a4
silky <noonsilk+-noonsilk@gmail.com>
2022-01-05 11:15:17 +0000

    initial

---
+++ src/Chapter3.hs
@@ -0,0 +1,20 @@
+module Chapter3 where
+
+import Data.Metrology
+import Data.Dimensions.SI
+
+
+{-
+  Question 1.
+
+  On a winter night, a person sits next to a window with an effective
+  temperature of 6⁰C.
+
+  - How much longwave radiation does the window emit?
+  
+  With a closed curtain the effective temperature is 18⁰C.
+
+  - How much longwave radiation is emitted? Assume an emisivity of one.
+  - Discuss why the person feels warmer with the curtain closed.
+-}
+q1 = undefined
